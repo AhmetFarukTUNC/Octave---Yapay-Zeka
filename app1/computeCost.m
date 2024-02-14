1020c2e941f9e0fecb636fcca00dedce01e0c762
Ahmet Faruk TUNC <129683968+AhmetFarukTUNC@users.noreply.github.com>
2024-02-14 18:49:07 +0300

    Update computeCost.m

--- app1/computeCost.m
+++ app1/computeCost.m
@@ -9,20 +9,12 @@ m = length(y); % number of training examples
 % You need to return the following variables correctly
 J = 0;
 
-% ====================== YOUR CODE HERE ======================
-% Instructions: Compute the cost of a particular choice of theta
-%               You should set J to the cost.
+
 
 predict = X * theta;
 
 squareError = (predict-y).^2;
 
-
 J = 1/(2*m)*sum(squareError);
-
-
-
-
-% =========================================================================
 
 end
